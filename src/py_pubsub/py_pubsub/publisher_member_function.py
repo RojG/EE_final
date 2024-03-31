6258321351656942ea72bce1918597ca1092af0a
RojG <jaigue134@gmail.com>
2024-03-31 07:32:15 -0700

    change the file with custom msg amd srv

--- src/py_pubsub/py_pubsub/publisher_member_function.py
+++ src/py_pubsub/py_pubsub/publisher_member_function.py
@@ -1,3 +1,42 @@
+# 3/31/2024 make some change of custom srv and msg file
+
+import rclpy
+from rclpy.node import Node
+
+from tutorial_interfaces.msg import Num    # CHANGE
+
+
+class MinimalPublisher(Node):
+
+    def __init__(self):
+        super().__init__('minimal_publisher')
+        self.publisher_ = self.create_publisher(Num, 'topic', 10)     # CHANGE
+        timer_period = 0.5
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        msg = Num()                                           # CHANGE
+        msg.num = self.i                                      # CHANGE
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%d"' % msg.num)  # CHANGE
+        self.i += 1
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_publisher = MinimalPublisher()
+
+    rclpy.spin(minimal_publisher)
+
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
+
 # Copyright 2016 Open Source Robotics Foundation, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,7 +50,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+'''
 import rclpy
 from rclpy.node import Node
 
@@ -56,3 +95,4 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+'''
